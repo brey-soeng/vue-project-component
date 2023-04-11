402331aea32d78bffca5916c4a5d7d1da8fb4158
soeng.brey <soeng.brey@ababank.com>
2023-04-11 16:16:03 +0700

    update darkmode component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
+  <div class="about px-10 dark:bg-black bg-white py-10 dark:text-white">
     <h1>This is an about page</h1>
+    <MarkMode />
   </div>
 </template>
+<script setup>
+import MarkMode from '@/components/DarkMode.vue'
+</script>

7ea36ca54bf57fcd1df8454096f56ff5da0485dc
soeng.brey <soeng.brey@ababank.com>
2023-04-24 17:24:05 +0700

    udpate

--- dist/assets/ArrowIcon-ff008a75.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{o as e,c as t,b as r}from"./index-422860db.js";const s={},c={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24"},n=r("path",{d:"M15,20H9V12H4.16L12,4.16L19.84,12H15V20Z",fill:"currentColor"},null,-1),_=[n];function a(l,i){return e(),t("svg",c,_)}const m=o(s,[["render",a]]);export{m as default};

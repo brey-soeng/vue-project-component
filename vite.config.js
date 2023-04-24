4bd21ca3f23eb604594c15129e02b09537550f4f
soeng.brey <soeng.brey@ababank.com>
2023-04-24 17:18:38 +0700

    udpate

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/tvc-components/',
+  base: '/vue-project-component/',
   server: {
     port: 3000
   },

3eea1ca3874daa2d7f834a342c80609bd988b57e
soeng.brey <soeng.brey@ababank.com>
2023-04-24 14:05:33 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  base: '/vue-project-component/',
+  base: '/tvc-components/',
   plugins: [
     vue(),
     VueI18nPlugin({

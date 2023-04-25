33f470b27b867f21106c7e387b4430080f79a2ce
soeng.brey <soeng.brey@ababank.com>
2023-04-25 09:28:49 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  base: '/vue-project-component/',
   plugins: [
     vue(),
     VueI18nPlugin({

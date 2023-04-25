4cee84e5b7c17f46641bf392a4df350ae4bae383
soeng.brey <soeng.brey@ababank.com>
2023-04-25 09:46:49 +0700

    add dist to gh-pages

--- src/icons/ArrowIcon.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path d="M15,20H9V12H4.16L12,4.16L19.84,12H15V20Z" fill="currentColor" />
-  </svg>
-</template>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <main>
-    <div class="text-red-500">Home</div>
-    <switch-language />
-    <svg-icon name="Home" size="100" />
-  </main>
-</template>

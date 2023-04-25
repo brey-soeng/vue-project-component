1f42a323126854dcbd33be94dc4c17d965a0349f
soeng.brey <soeng.brey@ababank.com>
2023-04-25 09:08:34 +0700

    add dist

---
+++ dist/assets/HomeView-6ccfadd2.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./_plugin-vue_export-helper-c27b6911.js";import{r as e,o as t,c as a,a as o,b as r}from"./index-d823e6db.js";const _={},m=r("div",{class:"text-red-500"},"Home",-1);function i(l,d){const n=e("switch-language"),c=e("svg-icon");return t(),a("main",null,[m,o(n),o(c,{name:"Home",size:"100"})])}const g=s(_,[["render",i]]);export{g as default};
